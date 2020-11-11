d49a50381c1c0bab8e3dd0adac3c5eaa58cd5f17
s-munro <68410363+s-munro@users.noreply.github.com>
2020-11-11 18:22:34 -0500

    added updatemovie, beginning form

--- client/src/App.js
+++ client/src/App.js
@@ -35,6 +35,8 @@ const App = () => {
       <Route path="/movies/:id">
         <Movie addToSavedList={addToSavedList} />
       </Route>
+
+      <Route path='/update-movie/:id' />
     </>
   );
 };
---
+++ client/src/components/UpdateMovie.js
@@ -0,0 +1,52 @@
+import react from 'react';
+
+const UpdateMovie = () => {
+
+
+
+  return(
+    <div>
+      <h2>Update Movie</h2>
+      <form onSubmit={}>
+
+        <label>Title
+          <input
+          type='text'
+          name=''
+          onChange={}
+          placeholder='text'
+          value={} />
+        </label>
+
+        <label>Director
+          <input
+            type='text'
+            name=''
+            onChange={}
+            placeholder='text'
+            value={} />
+        </label>
+
+        <label>Metascore
+          <input
+            type='text'
+            name=''
+            onChange={}
+            placeholder='text'
+            value={} />
+        </label>
+
+        <label>Stars
+          <input
+            type='text'
+            name=''
+            onChange={}
+            placeholder='text'
+            value={} />
+        </label>
+      </form>
+    </div>
+  )
+}
+
+export default UpdateMovie;
